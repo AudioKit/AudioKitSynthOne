62cfd3738c324143a6cfa1c80522b8cdaab9f001
Aurelius Prochazka <aure@aure.com>
2017-07-30 12:15:24 -0700

    Made the testing playground useful for visualizing tapers

--- iOS/AudioKitSynthOne/AudioKitSynthOne/Testing.playground/Contents.swift
+++ iOS/AudioKitSynthOne/AudioKitSynthOne/Testing.playground/Contents.swift
@@ -3,26 +3,20 @@
 import UIKit
 import AudioKit
 
-func scaleRangeLog(_ value: Double, rangeMin: Double, rangeMax: Double) -> Double {
-    let scale = (log(rangeMax) - log(rangeMin))
-    return exp(log(rangeMin) + (scale * value))
-}
-let a = 0.5
+var taper = 3.0
 
-
-var custom = AKTable(.sine, count: 1000)
+var custom = AKTable(.sine, count: 100)
 for i in custom.indices {
-    let val = Double(i)/1000
-    custom[i] = 
+    custom[i] = Float((Double(i)/100).denormalized(range: 0...10, taper: taper))
 }
 
 class PlaygroundView: AKPlaygroundView {
 
     override func setup() {
 
-        addTitle("Tables")
+        addTitle("Denormalization Playground")
 
-        addLabel("Custom")
+        addLabel("Taper = \(taper)")
         addSubview(AKTableView(custom))
     }
 }

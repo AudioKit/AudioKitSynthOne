9283f252a9d3763a1a416ce951f1f064e31a62af
Matthew Michael <matthew.fecher@gmail.com>
2017-08-08 16:50:50 -0400

    add buttons to top of keyboard

---
+++ iOS/AudioKitSynthOne/AudioKitSynthOne/Controls/Buttons/SynthUIButton.swift
@@ -0,0 +1,60 @@
+//
+//  SynthUIButton.swift
+//  AudioKitSynthOne
+//
+//  Created by Matthew Fecher on 8/8/17.
+//  Copyright © 2017 AudioKit. All rights reserved.
+//
+
+import UIKit
+
+class SynthUIButton: UIButton {
+        
+    private var isOn = false
+    
+    override var isSelected: Bool {
+        didSet {
+            isOn = isSelected
+            self.backgroundColor = isOn ? #colorLiteral(red: 0.368627451, green: 0.368627451, blue: 0.3882352941, alpha: 1) : #colorLiteral(red: 0.3058823529, green: 0.3058823529, blue: 0.3254901961, alpha: 1)
+            print ("isOn \(isOn)")
+        }
+    }
+    
+    var value: Double {
+        get {
+            return isOn ? 1 : 0
+        }
+        set {
+            isOn = value == 1.0
+        }
+    }
+    public var callback: (Double)->Void = { _ in }
+    
+    /*
+    override func draw(_ rect: CGRect) {
+        ToggleSwitchStyleKit.drawToggleSwitch(isToggled: isOn)
+    }
+    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        clipsToBounds = true
+        layer.cornerRadius = 1
+    }
+    
+    // *********************************************************
+    // MARK: - Handle Touches
+    // *********************************************************
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for _ in touches {
+            isSelected = !isSelected
+            self.setNeedsDisplay()
+            callback(value)
+        }
+    }
+    
+}
+

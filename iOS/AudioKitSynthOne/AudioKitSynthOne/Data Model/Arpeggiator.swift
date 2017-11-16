0b07829b6d89b95d6674f7868a30fcb000b40bd6
Marcus Hobbs <marcushobbs@mac.com>
2017-11-16 13:04:40 -0800

    commenting out Arpeggiator object...it is now unused.

--- iOS/AudioKitSynthOne/AudioKitSynthOne/Data Model/Arpeggiator.swift
+++ iOS/AudioKitSynthOne/AudioKitSynthOne/Data Model/Arpeggiator.swift
@@ -6,19 +6,20 @@
 //  Copyright © 2016 AudioKit. All rights reserved.
 //
 
-class Arpeggiator {
-    var baseNote = 0.0
-    var beatCounter = 0.0
-    var direction = 0.0
-    var interval = 12.0
-    var isOn = 0.0
-    var lastNotes = [0]
-    var octave = 1.0
-    var rate = 120.0
-    var isSequencer = false
-    var totalSteps = 8.0
-    var seqPattern = [0, 7, -2, 0, 0, 7, -2, 0, 0, 7, -2, 0, 0, 7, -2, 0]
-    var seqOctBoost = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
-    var seqNoteOn = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]
-    
-}
+//class Arpeggiator {
+//    var baseNote = 0.0
+//    var beatCounter = 0.0
+//    var direction = 0.0
+//    var interval = 12.0
+//    var isOn = 0.0
+//    var lastNotes = [0]
+//    var octave = 1.0
+//    var rate = 120.0
+//    var isSequencer = false
+//    var totalSteps = 8.0
+//    var seqPattern = [0, 7, -2, 0, 0, 7, -2, 0, 0, 7, -2, 0, 0, 7, -2, 0]
+//    var seqOctBoost = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
+//    var seqNoteOn = [true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]
+//
+//}
+

d44c82ff430570c9fb833975d199ec5b1e910525
Matthew Michael <matthew.fecher@gmail.com>
2017-12-02 00:54:55 -0500

    make dev panel UpdatableViewController

--- iOS/AudioKitSynthOne/AudioKitSynthOne/ViewControllers/DevViewController.swift
+++ iOS/AudioKitSynthOne/AudioKitSynthOne/ViewControllers/DevViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DevViewController: UIViewController {
+class DevViewController: UpdatableViewController {
     
     // *****************************************************************
     // MARK: - Lifecycle

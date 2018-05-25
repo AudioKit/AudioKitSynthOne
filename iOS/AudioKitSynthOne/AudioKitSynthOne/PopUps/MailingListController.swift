f5c7311c4ece2383a0b35912fbbfe29f442ac121
Aurelius Prochazka <aurephd@gmail.com>
2018-05-25 03:04:41 -0700

    Using self.count to get length of string
    Eliminates a few warnings

--- iOS/AudioKitSynthOne/AudioKitSynthOne/PopUps/MailingListController.swift
+++ iOS/AudioKitSynthOne/AudioKitSynthOne/PopUps/MailingListController.swift
@@ -154,7 +154,7 @@ extension String {
     var isEmail: Bool {
         do {
             let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
-            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil
+            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0,  self.count)) != nil
         } catch {
             return false
         }
@@ -168,9 +168,9 @@ extension String {
     var isValidPassword: Bool {
         do {
             let regex = try NSRegularExpression(pattern: "^[a-zA-Z_0-9\\-_,;.:#+*?=!§$%&/()@]+$", options: .caseInsensitive)
-            if(regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil){
+            if(regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil){
                 
-                if(self.characters.count>=6 && self.characters.count<=20){
+                if(self.count>=6 && self.count<=20){
                     return true
                 }else{
                     return false

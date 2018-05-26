9f352616901a186395a40b5fbf030da258d9507e
Marcus Hobbs <marcushobbs@mac.com>
2018-05-26 11:31:36 -0700

    resolved merge conflict

--- iOS/AudioKitSynthOne/AudioKitSynthOne/Data Model/AppSetting.swift
+++ iOS/AudioKitSynthOne/AudioKitSynthOne/Data Model/AppSetting.swift
@@ -124,53 +124,52 @@ class AppSetting: Codable {
         freezeReverb = dictionary["freezeReverb"] as? Bool ?? freezeReverb
         saveTuningWithPreset = dictionary["saveTuningWithPreset"] as? Bool ?? saveTuningWithPreset
         presetsVersion = dictionary["presetsVersion"] as? Double ?? presetsVersion
-        dspParamPortamentoHalfTime = dictionary["dspParamPortamentoHalfTime"] as? Double ?? dspParamPortamentoHalfTime
-        
-        masterVolume_CC = dictionary["masterVolume_CC"] as? Int ?? masterVolume_CC
-        morph1Selector_CC = dictionary["morph1Selector_CC"] as? Int ?? morph1Selector_CC
-        morph2Selector_CC = dictionary["morph2Selector_CC"] as? Int ?? morph2Selector_CC
-        morph1SemitoneOffset_CC = dictionary["morph1SemitoneOffset_CC"] as? Int ?? morph1SemitoneOffset_CC
-        morph2SemitoneOffset_CC = dictionary["morph2SemitoneOffset_CC"] as? Int ?? morph2SemitoneOffset_CC
-        morph2Detuning_CC = dictionary["morph2Detuning_CC"] as? Int ?? morph2Detuning_CC
-        morphBalance_CC = dictionary["morphBalance_CC"] as? Int ?? morphBalance_CC
-        morph1Volume_CC = dictionary["morph1Volume_CC"] as? Int ?? morph1Volume_CC
-        morph2Volume_CC = dictionary["morph2Volume_CC"] as? Int ?? morph2Volume_CC
-        
-        cutoff_CC = dictionary["cutoff_CC"] as? Int ?? cutoff_CC
-        rez_CC = dictionary["rez_CC"] as? Int ?? rez_CC
-        subVolume_CC = dictionary["subVolume_CC"] as? Int ?? subVolume_CC
-        fmVolume_CC = dictionary["fmVolume_CC"] as? Int ?? fmVolume_CC
-        noiseVolume_CC = dictionary["noiseVolume_CC"] as? Int ?? noiseVolume_CC
-        glideKnob_CC = dictionary["glideKnob_CC"] as? Int ?? glideKnob_CC
-        
-        arpInterval_CC = dictionary["arpInterval_CC"] as? Int ?? arpInterval_CC
-        
-        attackKnob_CC = dictionary["attackKnob_CC"] as? Int ?? attackKnob_CC
-        decayKnob_CC = dictionary["decayKnob_CC"] as? Int ?? decayKnob_CC
-        sustainKnob_CC = dictionary["sustainKnob_CC"] as? Int ?? sustainKnob_CC
-        releaseKnob_CC = dictionary["releaseKnob_CC"] as? Int ?? releaseKnob_CC
-        filterAttackKnob_CC = dictionary["filterAttackKnob_CC"] as? Int ?? filterAttackKnob_CC
-        filterDecayKnob_CC = dictionary["filterDecayKnob_CC"] as? Int ?? filterDecayKnob_CC
-        filterSustainKnob_CC = dictionary["filterSustainKnob_CC"] as? Int ?? filterSustainKnob_CC
-        filterReleaseKnob_CC = dictionary[" filterReleaseKnob_CC"] as? Int ?? filterReleaseKnob_CC
-        filterADSRMixKnob_CC = dictionary[" filterADSRMixKnob_CC"] as? Int ?? filterADSRMixKnob_CC
-        
-        sampleRate_CC = dictionary["sampleRate_CC"] as? Int ?? sampleRate_CC
-        delayTime_CC = dictionary["delayTime_CC"] as? Int ?? delayTime_CC
-        delayFeedback_CC = dictionary["delayFeedback_CC"] as? Int ?? delayFeedback_CC
-        delayMix_CC = dictionary["delayMix_CC"] as? Int ?? delayMix_CC
-        lfo1Amp_CC = dictionary["lfo1Amp_CC"] as? Int ?? lfo1Amp_CC
-        lfo2Amp_CC = dictionary["lfo2Amp_CC"] as? Int ?? lfo2Amp_CC
-        lfo1Rate_CC = dictionary["lfo1Rate_CC"] as? Int ?? lfo1Rate_CC
-        lfo2Rate_CC = dictionary["lfo2Rate_CC"] as? Int ?? lfo2Rate_CC
-        phaserMix_CC = dictionary["phaserMix_CC"] as? Int ?? phaserMix_CC
-        phaserRate_CC = dictionary["phaserRate_CC"] as? Int ?? phaserRate_CC
-        phaserFeedback_CC = dictionary["phaserFeedback_CC"] as? Int ?? phaserFeedback_CC
-        phaserNotchWidth_CC = dictionary["phaserNotchWidth_CC"] as? Int ?? phaserNotchWidth_CC
-        reverbSize_CC = dictionary["reverbSize_CC"] as? Int ?? reverbSize_CC
-        reverbLowCut_CC = dictionary["reverbLowCut_CC"] as? Int ?? reverbLowCut_CC
-        reverbMix_CC = dictionary["reverbMix_CC"] as? Int ?? reverbMix_CC
-        
+
+        masterVolumeCC = dictionary["masterVolumeCC"] as? Int ?? masterVolumeCC
+        morph1SelectorCC = dictionary["morph1SelectorCC"] as? Int ?? morph1SelectorCC
+        morph2SelectorCC = dictionary["morph2SelectorCC"] as? Int ?? morph2SelectorCC
+        morph1SemitoneOffsetCC = dictionary["morph1SemitoneOffsetCC"] as? Int ?? morph1SemitoneOffsetCC
+        morph2SemitoneOffsetCC = dictionary["morph2SemitoneOffsetCC"] as? Int ?? morph2SemitoneOffsetCC
+        morph2DetuningCC = dictionary["morph2DetuningCC"] as? Int ?? morph2DetuningCC
+        morphBalanceCC = dictionary["morphBalanceCC"] as? Int ?? morphBalanceCC
+        morph1VolumeCC = dictionary["morph1VolumeCC"] as? Int ?? morph1VolumeCC
+        morph2VolumeCC = dictionary["morph2VolumeCC"] as? Int ?? morph2VolumeCC
+
+        cutoffCC = dictionary["cutoffCC"] as? Int ?? cutoffCC
+        rezCC = dictionary["rezCC"] as? Int ?? rezCC
+        subVolumeCC = dictionary["subVolumeCC"] as? Int ?? subVolumeCC
+        fmVolumeCC = dictionary["fmVolumeCC"] as? Int ?? fmVolumeCC
+        noiseVolumeCC = dictionary["noiseVolumeCC"] as? Int ?? noiseVolumeCC
+        glideKnobCC = dictionary["glideKnobCC"] as? Int ?? glideKnobCC
+
+        arpIntervalCC = dictionary["arpIntervalCC"] as? Int ?? arpIntervalCC
+
+        attackKnobCC = dictionary["attackKnobCC"] as? Int ?? attackKnobCC
+        decayKnobCC = dictionary["decayKnobCC"] as? Int ?? decayKnobCC
+        sustainKnobCC = dictionary["sustainKnobCC"] as? Int ?? sustainKnobCC
+        releaseKnobCC = dictionary["releaseKnobCC"] as? Int ?? releaseKnobCC
+        filterAttackKnobCC = dictionary["filterAttackKnobCC"] as? Int ?? filterAttackKnobCC
+        filterDecayKnobCC = dictionary["filterDecayKnobCC"] as? Int ?? filterDecayKnobCC
+        filterSustainKnobCC = dictionary["filterSustainKnobCC"] as? Int ?? filterSustainKnobCC
+        filterReleaseKnobCC = dictionary[" filterReleaseKnobCC"] as? Int ?? filterReleaseKnobCC
+        filterADSRMixKnobCC = dictionary[" filterADSRMixKnobCC"] as? Int ?? filterADSRMixKnobCC
+
+        sampleRateCC = dictionary["sampleRateCC"] as? Int ?? sampleRateCC
+        delayTimeCC = dictionary["delayTimeCC"] as? Int ?? delayTimeCC
+        delayFeedbackCC = dictionary["delayFeedbackCC"] as? Int ?? delayFeedbackCC
+        delayMixCC = dictionary["delayMixCC"] as? Int ?? delayMixCC
+        lfo1AmpCC = dictionary["lfo1AmpCC"] as? Int ?? lfo1AmpCC
+        lfo2AmpCC = dictionary["lfo2AmpCC"] as? Int ?? lfo2AmpCC
+        lfo1RateCC = dictionary["lfo1RateCC"] as? Int ?? lfo1RateCC
+        lfo2RateCC = dictionary["lfo2RateCC"] as? Int ?? lfo2RateCC
+        phaserMixCC = dictionary["phaserMixCC"] as? Int ?? phaserMixCC
+        phaserRateCC = dictionary["phaserRateCC"] as? Int ?? phaserRateCC
+        phaserFeedbackCC = dictionary["phaserFeedbackCC"] as? Int ?? phaserFeedbackCC
+        phaserNotchWidthCC = dictionary["phaserNotchWidthCC"] as? Int ?? phaserNotchWidthCC
+        reverbSizeCC = dictionary["reverbSizeCC"] as? Int ?? reverbSizeCC
+        reverbLowCutCC = dictionary["reverbLowCutCC"] as? Int ?? reverbLowCutCC
+        reverbMixCC = dictionary["reverbMixCC"] as? Int ?? reverbMixCC
+
         // Keyboard
         labelMode = dictionary["labelMode"] as? Int ?? labelMode
         octaveRange = dictionary["octaveRange"] as? Int ?? octaveRange

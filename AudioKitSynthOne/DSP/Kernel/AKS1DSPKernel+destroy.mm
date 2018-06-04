769d06d0a5e100f88f61e9ed78b56fb92000aefe
Aurelius Prochazka <aurephd@gmail.com>
2018-06-04 03:10:02 -0700

    Possible way to start splitting up this big file

---
+++ AudioKitSynthOne/DSP/Kernel/AKS1DSPKernel+destroy.mm
@@ -0,0 +1,47 @@
+//
+//  AKS1DSPKernel+taper.mm
+//  AudioKitSynthOne
+//
+//  Created by Aurelius Prochazka on 6/4/18.
+//  Copyright © 2018 AudioKit. All rights reserved.
+//
+
+#import "AKS1DSPKernel.hpp"
+
+void AKS1DSPKernel::destroy() {
+    for(int i = 0; i< AKS1Parameter::AKS1ParameterCount; i++) {
+        if (aks1p[i].usePortamento) {
+            sp_port_destroy(&aks1p[i].portamento);
+        }
+    }
+    sp_port_destroy(&monoFrequencyPort);
+
+    sp_ftbl_destroy(&sine);
+    sp_phasor_destroy(&lfo1Phasor);
+    sp_phasor_destroy(&lfo2Phasor);
+    sp_phaser_destroy(&phaser0);
+    sp_osc_destroy(&panOscillator);
+    sp_pan2_destroy(&pan);
+    sp_moogladder_destroy(&loPassInputDelayL);
+    sp_moogladder_destroy(&loPassInputDelayR);
+    sp_vdelay_destroy(&delayL);
+    sp_vdelay_destroy(&delayR);
+    sp_vdelay_destroy(&delayRR);
+    sp_vdelay_destroy(&delayFillIn);
+    sp_delay_destroy(&widenDelay);
+    sp_crossfade_destroy(&delayCrossfadeL);
+    sp_crossfade_destroy(&delayCrossfadeR);
+    sp_revsc_destroy(&reverbCostello);
+    sp_buthp_destroy(&butterworthHipassL);
+    sp_buthp_destroy(&butterworthHipassR);
+    sp_crossfade_destroy(&revCrossfadeL);
+    sp_crossfade_destroy(&revCrossfadeR);
+    sp_compressor_destroy(&compressorMasterL);
+    sp_compressor_destroy(&compressorMasterR);
+    sp_compressor_destroy(&compressorReverbInputL);
+    sp_compressor_destroy(&compressorReverbInputR);
+    sp_compressor_destroy(&compressorReverbWetL);
+    sp_compressor_destroy(&compressorReverbWetR);
+    free(noteStates);
+    free(monoNote);
+}

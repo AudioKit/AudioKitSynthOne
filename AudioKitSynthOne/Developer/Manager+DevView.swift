fc0c70e1150c12dee5c9475c7207abd3642390e9
Marcus Hobbs <marcushobbs@mac.com>
2018-06-18 19:54:50 -0700

    updateDisplayLabel for dev controls

--- AudioKitSynthOne/Developer/Manager+DevView.swift
+++ AudioKitSynthOne/Developer/Manager+DevView.swift
@@ -20,17 +20,21 @@ extension Manager: DevViewDelegate {
 
     func freezeArpRateChanged(_ value: Bool) {
         appSettings.freezeArpRate = value
+        Conductor.sharedInstance.updateDisplayLabel("Freeze Arp Rate: \(value == false ? "false" : "true")")
     }
 
     func freezeReverbChanged(_ value: Bool) {
         appSettings.freezeReverb = value
+        Conductor.sharedInstance.updateDisplayLabel("Freeze Reverb: \(value == false ? "false" : "true")")
     }
 
     func freezeDelayChanged(_ value: Bool) {
         appSettings.freezeDelay = value
+        Conductor.sharedInstance.updateDisplayLabel("Freeze Delay: \(value == false ? "false" : "true")")
     }
 
     func portamentoChanged(_ value: Double) {
         appSettings.portamentoHalfTime = value
+        Conductor.sharedInstance.updateDisplayLabel("dsp smoothing half time: \(value.decimalString)")
     }
 }

e0cd411eebc09831e180f9a872c6b75bef702e13
Marcus Hobbs <marcushobbs@mac.com>
2019-03-10 10:34:54 -0700

    oscBandlimitIndexOverride no longer needed
    * Removed oscBandlimitIndexOverride functionality+storyboards (no longer needed)
    * oscBandlimitIndexOverride is still a DSP param, but it’s unused

--- AudioKitSynthOne/Developer/DevViewController.swift
+++ AudioKitSynthOne/Developer/DevViewController.swift
@@ -55,11 +55,11 @@ class DevViewController: UpdatableViewController {
     @IBOutlet weak var freezeArpSeq: ToggleButton!
     var freezeArpSeqValue = false
 
+    @IBOutlet weak var whiteKeysOnly: ToggleButton!
+    var whiteKeysOnlyValue = false
+    
     @IBOutlet weak var portamento: Knob!
     var portamentoHalfTime = 0.1
-
-    @IBOutlet weak var oscBandlimitIndexOverrideKnob: Knob!
-    @IBOutlet weak var oscBandlimitEnable: ToggleButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -165,10 +165,10 @@ class DevViewController: UpdatableViewController {
             self.delegate?.portamentoChanged(value)
         }
 
-        //
-        oscBandlimitIndexOverrideKnob.range = s.getRange(.oscBandlimitIndexOverride)
-        conductor.bind(oscBandlimitIndexOverrideKnob, to: .oscBandlimitIndexOverride)
-        conductor.bind(oscBandlimitEnable, to: .oscBandlimitEnable)
+        // TODO: flesh this out
+        whiteKeysOnly.callback = { value in
+            AKLog("whiteKeysOnly")
+        }
 
         setupLinkStuff()
     }

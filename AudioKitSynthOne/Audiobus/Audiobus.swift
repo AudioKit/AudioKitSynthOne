AudioKit/AudioKitSynthOne

commit a64d636b6cd17f23bfafe02456ae5b94ead15141

Don't compile AudioBus/IAA on Cataylst
Remove Audiobus at compile-time when compiling
for macOS Cataylst.

AudioKitSynthOne/Audiobus/Audiobus.swift
@@ -12,6 +12,7 @@ import Foundation
 import AudioKit
 import CoreAudio
 
+#if !targetEnvironment(macCatalyst)
 class Audiobus {
 
     // MARK: Client
@@ -113,8 +114,9 @@ class Audiobus {
     }
 
     var isConnectedToInput: Bool {
-        return controller.isConnectedToAudiobus(portOfType: ABPortTypeAudioSender) ||
-            audioUnit.isConnectedToInterAppAudio(nodeOfType: kAudioUnitType_RemoteEffect)
+        var iaaConnected = false
+        iaaConnected = audioUnit.isConnectedToInterAppAudio(nodeOfType: kAudioUnitType_RemoteEffect)
+        return controller.isConnectedToAudiobus(portOfType: ABPortTypeAudioSender) || iaaConnected
     }
 
     // MARK: Connections
@@ -127,7 +129,6 @@ class Audiobus {
         audioUnitPropertyListener = AudioUnitPropertyListener { (_, _) in
             self.updateConnections()
         }
-
         try! audioUnit.add(listener: audioUnitPropertyListener, toProperty: kAudioUnitProperty_IsInterAppConnected)
     }
 
@@ -175,13 +176,30 @@ private extension ABAudiobusController {
 private extension AudioUnit {
 
     var isConnectedToInterAppAudio: Bool {
+        return false
         let value: UInt32 = try! getValue(forProperty: kAudioUnitProperty_IsInterAppConnected)
         return value != 0
     }
 
     func isConnectedToInterAppAudio(nodeOfType type: OSType) -> Bool {
+        return false
         let value: AudioComponentDescription = try! getValue(forProperty: kAudioOutputUnitProperty_NodeComponentDescription)
         return value.componentType == type
     }
 
 }
+#endif
+
+// Stubs for MacOS Cataylst target
+// Does nothing
+#if targetEnvironment(macCatalyst)
+protocol ABAudiobusControllerStateIODelegate {}
+class Audiobus {
+  class ABAudiobusController{
+    var stateIODelegate: ABAudiobusControllerStateIODelegate?
+  }
+
+  static var client: Audiobus?
+  var controller: ABAudiobusController!
+}
+#endif

AudioKitSynthOne.xcodeproj/project.pbxproj
@@ -9,6 +9,7 @@
 /* Begin PBXBuildFile section */
 		48C3EE7437FA1B1E7916844E /* Pods_AudioKitSynthOne.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = BA27CDAC229BF66A1C649B42 /* Pods_AudioKitSynthOne.framework */; };
 		69115F3E2363BBA100B144A5 /* Conductor+Audiobus.swift in Sources */ = {isa = PBXBuildFile; fileRef = 69115F3D2363BBA000B144A5 /* Conductor+Audiobus.swift */; };
+		6982467D236EDCE600407DF4 /* Conductor+Platform.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6982467C236EDCE600407DF4 /* Conductor+Platform.swift */; };
 		69C11CF7236C96DA00833BF7 /* MailchimpService.swift in Sources */ = {isa = PBXBuildFile; fileRef = 690F807A236B5B9D00FDC518 /* MailchimpService.swift */; };
 		69C11CF8236C96DC00833BF7 /* MD5Helpers.swift in Sources */ = {isa = PBXBuildFile; fileRef = 69C11CF3236C375B00833BF7 /* MD5Helpers.swift */; };
 		941645CD20B3597300D62851 /* NotificationService.swift in Sources */ = {isa = PBXBuildFile; fileRef = 941645CC20B3597300D62851 /* NotificationService.swift */; };
@@ -302,6 +303,7 @@
 		5FAA47B316A5F659F440DAC9 /* Pods-AKS1Extension.debug.xcconfig */ = {isa = PBXFileReference; includeInIndex = 1; lastKnownFileType = text.xcconfig; name = "Pods-AKS1Extension.debug.xcconfig"; path = "Pods/Target Support Files/Pods-AKS1Extension/Pods-AKS1Extension.debug.xcconfig"; sourceTree = "<group>"; };
 		690F807A236B5B9D00FDC518 /* MailchimpService.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MailchimpService.swift; sourceTree = "<group>"; };
 		69115F3D2363BBA000B144A5 /* Conductor+Audiobus.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = "Conductor+Audiobus.swift"; sourceTree = "<group>"; };
+		6982467C236EDCE600407DF4 /* Conductor+Platform.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "Conductor+Platform.swift"; sourceTree = "<group>"; };
 		69C11CE4236C2F3700833BF7 /* AudioKit For iOS.xcodeproj */ = {isa = PBXFileReference; lastKnownFileType = "wrapper.pb-project"; name = "AudioKit For iOS.xcodeproj"; path = "../AudioKit/AudioKit/iOS/AudioKit For iOS.xcodeproj"; sourceTree = "<group>"; };
 		69C11CF3236C375B00833BF7 /* MD5Helpers.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = MD5Helpers.swift; sourceTree = "<group>"; };
 		69E4E45622381DF90049F0EA /* S1DSPCompressor.hpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.h; path = S1DSPCompressor.hpp; sourceTree = "<group>"; };
@@ -1005,6 +1007,7 @@
 				F437E99E20EB22D00096FEAC /* README.md */,
 				C435C55420C530C900DAECCD /* AKSynthOne.swift */,
 				C435C55120C530C900DAECCD /* Conductor.swift */,
+				6982467C236EDCE600407DF4 /* Conductor+Platform.swift */,
 				F4B1AD64228BDCEC009FA6BA /* S1TuningTable.swift */,
 				F4CA406C224EB2A70053F29E /* S1Control.swift */,
 				C435C55320C530C900DAECCD /* S1Parameter.h */,
@@ -1822,6 +1825,7 @@
 				C4B491A120C7C91A00FD565A /* EnvelopesPanelController.swift in Sources */,
 				94F7846722D3FE8400C35702 /* SearchViewController.swift in Sources */,
 				94D917AD1F550D0E006ACB4E /* FlatToggleButtonStyleKit.swift in Sources */,
+				6982467D236EDCE600407DF4 /* Conductor+Platform.swift in Sources */,
 				F40636FF22E3EECB00A422D3 /* KeyboardView+Draw12ET.swift in Sources */,
 				C432979320CB2BB20063F795 /* Presets+LoadSaveManipulate.swift in Sources */,
 				F491FC5422DF8BDF00CDB78E /* Tunings+Drawing.swift in Sources */,

AudioKitSynthOne/Audiobus/Audiobus+MIDI.swift
@@ -9,6 +9,7 @@
 import Foundation
 import AudioKit
 
+#if !targetEnvironment(macCatalyst)
 extension Audiobus {
 
     // MARK: - Preparations
@@ -67,3 +68,4 @@ extension Audiobus {
         ABMIDIPortSendPacketList(midiSendPort, UnsafePointer(packetListPointer))
     }
 }
+#endif

AudioKitSynthOne/Audiobus/Conductor+Audiobus.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+#if !targetEnvironment(macCatalyst)
 extension Conductor {
 
   func setupAudioBusInput() {
@@ -50,3 +51,11 @@ extension Conductor {
         Audiobus.client?.controller.addMIDIReceiverPort(midiInput)
     }
 }
+#endif
+
+// Stubs for macOS Cataylst
+#if targetEnvironment(macCatalyst)
+extension Conductor {
+    func setupAudioBusInput() { }
+}
+#endif

AudioKitSynthOne/Audiobus/Manager+Audiobus.swift
@@ -7,7 +7,7 @@
 //
 
 // AudioBus MIDI Input & Preset Loading
-
+#if !targetEnvironment(macCatalyst)
 extension Manager: ABAudiobusControllerStateIODelegate {
 
     // MARK: - AudioBus Preset Delegate
@@ -27,3 +27,10 @@ extension Manager: ABAudiobusControllerStateIODelegate {
         }
     }
 }
+#endif
+
+// Stubs for MacOS Catalyst target
+
+#if targetEnvironment(macCatalyst)
+extension Manager: ABAudiobusControllerStateIODelegate { }
+#endif

AudioKitSynthOne/DSP/AudioKitSynthOne-Bridging-Header.h
@@ -11,8 +11,9 @@
 #import "S1AudioUnit.h"
 #import "S1Parameter.h"
 #import "AKSynthOneRate.h"
+#if !TARGET_OS_MACCATALYST
 #import "Audiobus.h"
-
+#endif
 // Set the ABLETON_ENABLED user setting to 1 (at the project level) to enable Ableton Link support
 // Note: you will need the files from their SDK!
 #if ABLETON_ENABLED

AudioKitSynthOne/DSP/Conductor+Platform.swift
@@ -1,0 +1,46 @@
+//
+//  Conductor+Platform.swift
+//  AudioKitSynthOne
+//
+//  Created by Matthias Frick on 03/11/2019.
+//  Copyright © 2019 AudioKit. All rights reserved.
+//
+
+import Foundation
+
+#if !targetEnvironment(macCatalyst)
+extension Conductor {
+      @objc func checkIAAConnectionsEnterBackground() {
+
+          if let audiobusClient = Audiobus.client {
+
+              if !audiobusClient.isConnected && !audiobusClient.isConnectedToInput && !backgroundAudio {
+                  deactivateSession()
+                  AKLog("disconnected without timer")
+              } else {
+                  iaaTimer.invalidate()
+                  iaaTimer = Timer.scheduledTimer(timeInterval: 20 * 60,
+                                                  target: self,
+                                                  selector: #selector(self.checkIAAConnectionsEnterBackground),
+                                                  userInfo: nil, repeats: true)
+              }
+          }
+
+      }
+
+      func checkIAAConnectionsEnterForeground() {
+          iaaTimer.invalidate()
+          startEngine()
+      }
+}
+#endif
+
+// MacOS Cataylyst stubs
+// We don't support IAA there so adding NOOPs
+#if targetEnvironment(macCatalyst)
+extension Conductor {
+      @objc func checkIAAConnectionsEnterBackground() { }
+
+      func checkIAAConnectionsEnterForeground() { }
+}
+#endif

AudioKitSynthOne/DSP/Conductor.swift
@@ -31,7 +31,9 @@ class Conductor: S1Protocol {
 
     var sustainer: SDSustainer!
 
+  #if !targetEnvironment(macCatalyst)
     var midiInput: ABMIDIReceiverPort?
+  #endif
 
     var audioBusMidiDelegate: AKMIDIListener?
 
@@ -215,6 +217,7 @@ class Conductor: S1Protocol {
         }
         started = true
 
+        #if !targetEnvironment(macCatalyst)
         if let au = AudioKit.engine.outputNode.audioUnit {
             // IAA Host Icon
             audioUnitPropertyListener = AudioUnitPropertyListener { (_, _) in
@@ -233,6 +236,7 @@ class Conductor: S1Protocol {
         }
         Audiobus.start()
         setupAudioBusInput()
+        #endif
     }
 
     func updateDisplayLabel(_ message: String) {
@@ -307,28 +311,6 @@ class Conductor: S1Protocol {
 
     func stopEngine() {
         AudioKit.engine.pause()
-    }
-
-    @objc func checkIAAConnectionsEnterBackground() {
-
-        if let audiobusClient = Audiobus.client {
-
-            if !audiobusClient.isConnected && !audiobusClient.isConnectedToInput && !backgroundAudio {
-                deactivateSession()
-                AKLog("disconnected without timer")
-            } else {
-                iaaTimer.invalidate()
-                iaaTimer = Timer.scheduledTimer(timeInterval: 20 * 60,
-                                                target: self,
-                                                selector: #selector(self.checkIAAConnectionsEnterBackground),
-                                                userInfo: nil, repeats: true)
-            }
-        }
-    }
-
-    func checkIAAConnectionsEnterForeground() {
-        iaaTimer.invalidate()
-        startEngine()
     }
 
     func deactivateSession() {

commit efca914e36d7aaba53f8209ac5b65de84900977f

Fix IAA Bug from Catalina Work
Introduced this in a64d636b6cd17f23bfafe02456ae5b94ead15141 accidentally
while comitting a bunch of Catalina prep work to the repo.

AudioKitSynthOne/Audiobus/Audiobus.swift
@@ -176,13 +176,11 @@ private extension ABAudiobusController {
 private extension AudioUnit {
 
     var isConnectedToInterAppAudio: Bool {
-        return false
         let value: UInt32 = try! getValue(forProperty: kAudioUnitProperty_IsInterAppConnected)
         return value != 0
     }
 
     func isConnectedToInterAppAudio(nodeOfType type: OSType) -> Bool {
-        return false
         let value: AudioComponentDescription = try! getValue(forProperty: kAudioOutputUnitProperty_NodeComponentDescription)
         return value.componentType == type
     }
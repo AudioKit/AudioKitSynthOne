8b119f7d728efec89c2362dcb197d26cb0f4040c
Marcus Hobbs <marcushobbs@mac.com>
2020-02-19 23:02:55 -0800

    whitespace

---
+++ AudioKitSynthOne/Manager/Manager+Notifications.swift
@@ -0,0 +1,9 @@
+//
+//  Manager+Notifications.swift
+//  AudioKitSynthOne
+//
+//  Created by Marcus W. Hobbs on 2/19/20.
+//  Copyright © 2020 AudioKit. All rights reserved.
+//
+
+import Foundation
